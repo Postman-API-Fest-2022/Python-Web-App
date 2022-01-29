48f4a5d83c78f3622e97712fe9d8f3e7e97e2d15
Saptak10 <saptak.bugatti20@gmail.com>
2022-01-29 13:26:11 +0530

    folder name change

---
+++ backend/src/main/java/com/example/student/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.example.student.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.student.model.Student;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Integer>{
+
+}
